8e4b158941a4f1166e25e35eab9121ba50927917
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-07 13:26:04 +0100

    commenting out EH names for time being

--- components/aria-layer/eventhub.tf
+++ components/aria-layer/eventhub.tf
@@ -1,4 +1,4 @@
-resource "azurerm_eventhub_namespace" "this" {
+/* resource "azurerm_eventhub_namespace" "this" {
   name                 = length("${local.name}-integration-eventHubNamespace001-${var.env}") > 50 ? "${local.short_name}-integration-eventHubNamespace001-${var.env}" : "${local.name}-integration-eventHubNamespace001-${var.env}"
   location             = var.location
   resource_group_name  = azurerm_resource_group.this[local.shared_integration_resource_group].name
@@ -6,5 +6,5 @@ resource "azurerm_eventhub_namespace" "this" {
   capacity             = 1
   auto_inflate_enabled = false
 }
-
+ */
 # create link to namespace, for each lz, pull eh namespace per entity
