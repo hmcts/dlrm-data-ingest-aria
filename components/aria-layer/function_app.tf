7945eee7bf908fe0eb224f003edc3c9a45e746e0
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-09 09:14:43 +0100

    Adding functions_worker_runtime to azurefunction IAC

--- components/aria-layer/function_app.tf
+++ components/aria-layer/function_app.tf
@@ -31,12 +31,14 @@ resource "azurerm_linux_function_app" "example" {
 
   site_config {
     always_on = false
+    
   }
 
   app_settings = {
     APPLICATIONINSIGHTS_CONNECTION_STRING                 = azurerm_application_insights.example[each.key].connection_string
     BUILD_FLAGS                                           = "UseExpressBuild"
     ENABLE_ORYX_BUILD                                     = true
+    FUNCTIONS_WORKER_RUNTIME                              = "python"
     sboxdlrmeventhubns_RootManageSharedAccessKey_EVENTHUB = data.azurerm_eventhub_namespace_authorization_rule.lz[each.value.lz_key].primary_connection_string
     SCM_DO_BUILD_DURING_DEPLOYMENT                        = 1
     XDG_CACHE_HOME                                        = "/tmp/.cache"
