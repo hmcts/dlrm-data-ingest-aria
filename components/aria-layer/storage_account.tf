8fc7a52fb4be5e21ef8a240af452f14288803ff4
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-12 11:28:13 +0100

    Adding storage account.tf

--- components/aria-layer/storage_account.tf
+++ components/aria-layer/storage_account.tf
@@ -1,14 +1,14 @@
 
 #Storage account is required for function_app provisioning
 
-/* resource "azurerm_storage_account" "example" {
+ resource "azurerm_storage_account" "example" {
   for_each = {
     for app in local.flattened_function_apps :
-    "${app.lz_key}-${app.name}" => app
+    "${app.lz_key}-${app.base_name}" => app
     if !(var.env == "sbox" && app.lz_key == "00")
   }
 
-  name                     = replace(each.value.name, "-", "")
+  name                     = replace(each.value.full_name, "-", "")
   resource_group_name      = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].name
   location                 = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
   account_tier             = "Standard"
@@ -16,4 +16,4 @@
 
   tags = module.ctags.common_tags
 }
- */
+ 
