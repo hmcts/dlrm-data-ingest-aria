2e12df171b8bad9a7e59ec75cfc1322e43617776
araislam-solirius <163121870+araislam-solirius@users.noreply.github.com>
2025-05-12 17:44:19 +0100

    testing if we can access curates storage acc 4

--- components/aria-layer/storage_account.tf
+++ components/aria-layer/storage_account.tf
@@ -1,6 +1,18 @@
 
 #Storage account is required for function_app provisioning
 
+locals {
+  container_name = ["bronze", "silver", "gold"]
+
+  container_matrix = {
+    for pair in setproduct(keys(var.landing_zones), local.container_name) :
+    "${pair[0]}-${paor[1]}" => {
+      lz_key         = pair[0]
+      container_name = pair[1]
+    } if pair[0] != "00"
+  }
+}
+
 resource "azurerm_storage_account" "example" {
   for_each = {
     for app in local.flattened_function_apps :
@@ -26,9 +38,9 @@ data "azurerm_storage_account" "curated" {
 }
 
 resource "azurerm_storage_container" "curated_extra" {
-  for_each = var.landing_zones
+  for_each = local.container_matrix
 
-  name                  = "test-container-iac"
-  storage_account_name  = data.azurerm_storage_account.curated[each.key].name
+  name                  = each.value.container_name
+  storage_account_name  = data.azurerm_storage_account.curated[each.value.lz_key].name
   container_access_type = "private"
 }
