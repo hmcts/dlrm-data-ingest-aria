271ba50aea1f6e2b2ebd6dc52c3ff4344f26e883
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-15 09:15:30 +0100

    fixing typo

--- components/aria-layer/function_app.tf
+++ components/aria-layer/function_app.tf
@@ -28,7 +28,7 @@ resource "azurerm_linux_function_app" "example" {
   location                   = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
   service_plan_id            = azurerm_service_plan.example[each.key].id
   storage_account_name       = azurerm_storage_account.example[each.key].name #data.azurerm_storage_account.xcutting[each.value.lz_key].name
-  storage_account_access_key = azurerm_storage_account.example[each.key].name #data.azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key
+  storage_account_access_key = azurerm_storage_account.example[each.key].primary_access_key #data.azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key
 
   app_settings = {
     APPLICATIONINSIGHTS_CONNECTION_STRING                 = azurerm_application_insights.example[each.key].connection_string
