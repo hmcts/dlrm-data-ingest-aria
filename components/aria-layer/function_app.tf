2923ff5a3eb33df6990c1c418dc4234ce707b767
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-15 09:18:23 +0100

    linting fix

--- components/aria-layer/function_app.tf
+++ components/aria-layer/function_app.tf
@@ -27,7 +27,7 @@ resource "azurerm_linux_function_app" "example" {
   resource_group_name        = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].name
   location                   = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
   service_plan_id            = azurerm_service_plan.example[each.key].id
-  storage_account_name       = azurerm_storage_account.example[each.key].name #data.azurerm_storage_account.xcutting[each.value.lz_key].name
+  storage_account_name       = azurerm_storage_account.example[each.key].name               #data.azurerm_storage_account.xcutting[each.value.lz_key].name
   storage_account_access_key = azurerm_storage_account.example[each.key].primary_access_key #data.azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key
 
   app_settings = {
