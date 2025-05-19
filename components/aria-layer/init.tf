41ed74b2010402640a3fb9cf4de6a6e15c5b16ee
araislam-solirius <163121870+araislam-solirius@users.noreply.github.com>
2025-05-19 16:34:34 +0100

    validating a test for the variable group

--- components/aria-layer/init.tf
+++ components/aria-layer/init.tf
@@ -23,11 +23,15 @@ provider "azurerm" {
 provider "databricks" {
   alias                       = "sbox-00"
   azure_workspace_resource_id = data.azurerm_databricks_workspace.ws["sbox-00"].id
-  use_azure_cli_auth          = true
+  client_id                   = var.client_id_test
+  client_secret               = var.client_secret_test
+  tenant_id                   = var.tenant_id_test
 }
 
-provider "databricks" {
-  alias                       = "sbox-02"
-  azure_workspace_resource_id = data.azurerm_databricks_workspace.ws["sbox-02"].id
-  use_azure_cli_auth          = true
-}
+# provider "databricks" {
+#   alias                       = "sbox-02"
+#   azure_workspace_resource_id = data.azurerm_databricks_workspace.ws["sbox-02"].id
+#   client_id                   = var.client_id
+#   client_secret               = var.client_secret
+#   tenant_id                   = var.tenant_id
+# }
--- components/aria-layer/variables.tf
+++ components/aria-layer/variables.tf
@@ -16,3 +16,37 @@ variable "eventhub_topic_suffixes" {
   type        = list(string)
   default     = ["pub", "ack", "dl"]
 }
+
+# variable "db_workspace_configs" {
+#   type = map(object({
+#     databricks_ws_name = string
+#     databriks_rg_name = string
+#     key_vault_name = string
+#     key_vault_rg_name = string
+#   }))
+
+# }
+
+variable "client_id_test" {
+  description = "Pulling the client ID fromt the Azure Devops Pipeline variable group"
+
+  type = string
+  default = $(client_id_test)
+
+}
+
+variable "client_secret_test" {
+  description = "Pulling the client ID fromt the Azure Devops Pipeline variable group"
+  
+  type = string
+  default = $(client_secret_test)
+
+}
+
+variable "tenant_id_test" {
+  description = "Pulling the client ID fromt the Azure Devops Pipeline variable group"
+  
+  type = string
+  default = $(tenant_id_test)
+
+}
