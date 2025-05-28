0cfc67ac69aaa91712851bf8bc9f0086d372602c
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-28 09:49:37 +0100

    Fixing typo from sbox-oo to prod-oo

--- components/aria-layer/init.tf
+++ components/aria-layer/init.tf
@@ -62,8 +62,8 @@ provider "databricks" {
 
 provider "databricks" {
   alias                       = "prod-00"
-  azure_workspace_resource_id = data.azurerm_databricks_workspace.db_ws["sbox-00"].id
-  host                        = data.azurerm_databricks_workspace.db_ws["sbox-00"].workspace_url
+  azure_workspace_resource_id = data.azurerm_databricks_workspace.db_ws["prod-00"].id
+  host                        = data.azurerm_databricks_workspace.db_ws["prod-00"].workspace_url
 
   azure_client_id     = var.ClientId 
   azure_client_secret = var.ClientSecret # TODO: for prod, request client secret value from platops
