9346838006f33f503dd38697d4bed48c1fd9afdb
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-28 10:12:56 +0100

    Removing provider databricks for stg/prod. Adding normal Storage Accounts for new Azurefunctions. To update to xcutting once provisioned

--- components/aria-layer/init.tf
+++ components/aria-layer/init.tf
@@ -50,22 +50,22 @@ provider "databricks" {
   azure_tenant_id     = var.TenantId
 }
 
-provider "databricks" {
-  alias                       = "stg-00"
-  azure_workspace_resource_id = data.azurerm_databricks_workspace.db_ws["stg-00"].id
-  host                        = data.azurerm_databricks_workspace.db_ws["stg-00"].workspace_url
+# provider "databricks" {
+#   alias                       = "stg-00"
+#   azure_workspace_resource_id = data.azurerm_databricks_workspace.db_ws["stg-00"].id
+#   host                        = data.azurerm_databricks_workspace.db_ws["stg-00"].workspace_url
 
-  azure_client_id     = var.ClientId
-  azure_client_secret = var.ClientSecret # TODO: for stg, request client secret value from platops
-  azure_tenant_id     = var.TenantId
-}
+#   azure_client_id     = var.ClientId
+#   azure_client_secret = var.ClientSecret # TODO: for stg, request client secret value from platops
+#   azure_tenant_id     = var.TenantId
+# }
 
-provider "databricks" {
-  alias                       = "prod-00"
-  azure_workspace_resource_id = data.azurerm_databricks_workspace.db_ws["prod-00"].id
-  host                        = data.azurerm_databricks_workspace.db_ws["prod-00"].workspace_url
+# provider "databricks" {
+#   alias                       = "prod-00"
+#   azure_workspace_resource_id = data.azurerm_databricks_workspace.db_ws["prod-00"].id
+#   host                        = data.azurerm_databricks_workspace.db_ws["prod-00"].workspace_url
 
-  azure_client_id     = var.ClientId
-  azure_client_secret = var.ClientSecret # TODO: for prod, request client secret value from platops
-  azure_tenant_id     = var.TenantId
-}
+#   azure_client_id     = var.ClientId
+#   azure_client_secret = var.ClientSecret # TODO: for prod, request client secret value from platops
+#   azure_tenant_id     = var.TenantId
+# }
