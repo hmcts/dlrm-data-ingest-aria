047ff9c3d065326fbe257e095df429191fdc664c
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-05-28 13:34:25 +0100

    Adding vriables to ./environments

---
+++ environments/stg/variables.tf
@@ -0,0 +1,24 @@
+variable "ClientId" {
+  type    = string
+  default = ""
+}
+
+variable "ClientSecret" {
+  type    = string
+  default = ""
+}
+
+variable "TenantId" {
+  type    = string
+  default = ""
+}
+
+variable "TenantURL" {
+  type    = string
+  default = ""
+}
+
+variable "environment" {
+  type    = string
+  default = ""
+}
