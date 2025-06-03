68716c5d3acc8e174cd816d28fc45dd2d36fa056
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-06-03 17:56:05 +0100

    Removing hard-coded value for dynamic field

--- components/aria-layer/key_vault.tf
+++ components/aria-layer/key_vault.tf
@@ -76,7 +76,7 @@ resource "azurerm_key_vault_secret" "curated_sas_token" {
   for_each = var.landing_zones
 
   name         = "CURATED-${var.env}-SAS-TOKEN-TEST"
-  value        = "sv=2022-11-02&ss=b&srt=s&sp=rwdlac&se=2026-03-21T00:00:00Z&st=2025-03-21T00:00:00Z&spr=https&sig=pc49qK11QhgnUCObd4LHoFOtbGj%2FsWoJGJ0qkqfAm0k%3D"
+  value        = "${data.azurerm_storage_account_sas.curated[each.key].sas}"
   key_vault_id = data.azurerm_key_vault.logging_vault[each.key].id
 }
 
