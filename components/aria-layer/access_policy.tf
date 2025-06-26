888e13f912fb9a19a2cff6675d5deb75c8ed2f4c
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-06-26 11:27:53 +0100

    Commenting out dependency on af

--- components/aria-layer/access_policy.tf
+++ components/aria-layer/access_policy.tf
@@ -1,24 +1,24 @@
-resource "azurerm_key_vault_access_policy" "example-principal" {
-  for_each = azurerm_linux_function_app.example
+# resource "azurerm_key_vault_access_policy" "example-principal" {
+#   for_each = azurerm_linux_function_app.example
 
-  key_vault_id = data.azurerm_key_vault.logging_vault[split("-", each.key)[0]].id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = each.value.identity[0].principal_id
+#   key_vault_id = data.azurerm_key_vault.logging_vault[split("-", each.key)[0]].id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = each.value.identity[0].principal_id
 
-  key_permissions = [
-    "Get", "List", "Encrypt", "Decrypt", "Backup", "Create", "Delete", "Import", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate"
-  ] #Backup, Create, Decrypt, Delete, Encrypt, Get, Import, List, Purge, Recover, Restore, Sign, UnwrapKey, Update, Verify, WrapKey, Release, Rotate, GetRotationPolicy and SetRotationPolicy.
+#   key_permissions = [
+#     "Get", "List", "Encrypt", "Decrypt", "Backup", "Create", "Delete", "Import", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate"
+#   ] #Backup, Create, Decrypt, Delete, Encrypt, Get, Import, List, Purge, Recover, Restore, Sign, UnwrapKey, Update, Verify, WrapKey, Release, Rotate, GetRotationPolicy and SetRotationPolicy.
 
-  secret_permissions = [
-    "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
-  ] #Backup, Delete, Get, List, Purge, Recover, Restore and Set
+#   secret_permissions = [
+#     "Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"
+#   ] #Backup, Delete, Get, List, Purge, Recover, Restore and Set
 
-  storage_permissions = [
-    "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"
-  ] #Backup, Delete, DeleteSAS, Get, GetSAS, List, ListSAS, Purge, Recover, RegenerateKey, Restore, Set, SetSAS and Update.
+#   storage_permissions = [
+#     "Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"
+#   ] #Backup, Delete, DeleteSAS, Get, GetSAS, List, ListSAS, Purge, Recover, RegenerateKey, Restore, Set, SetSAS and Update.
 
-  depends_on = [azurerm_linux_function_app.example]
-}
+#   depends_on = [azurerm_linux_function_app.example]
+# }
 
 
 # Below assigns RBAC permissions to use the Service Principle (object_id) access to Contribute/Own permissions to the storage accounts highlighted in locals below.
