18a885484f367017aa85a8213478b60d09514393
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-06-26 16:50:05 +0100

    commenting out website app settings

--- components/aria-layer/function_app.tf
+++ components/aria-layer/function_app.tf
@@ -16,58 +16,59 @@ resource "azurerm_service_plan" "example" {
 }
 
 # Define the function app 
-# resource "azurerm_linux_function_app" "example" {
-#   for_each = {
-#     for app in local.flattened_function_apps :
-#     "${app.lz_key}-${app.base_name}" => app
-#   }
+resource "azurerm_linux_function_app" "example" {
+  for_each = {
+    for app in local.flattened_function_apps :
+    "${app.lz_key}-${app.base_name}" => app
+  }
 
-#   name                       = each.value.full_name
-#   resource_group_name        = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].name
-#   location                   = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
-#   service_plan_id            = azurerm_service_plan.example[each.key].id
-#   storage_account_name       = data.azurerm_storage_account.xcutting[each.value.lz_key].name                       #azurerm_storage_account.example[each.key].name        
-#   storage_account_access_key = data.azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key         #azurerm_storage_account.example[each.key].primary_access_key
-#   virtual_network_subnet_id  = data.azurerm_subnet.lz["ingest${each.value.lz_key}-data-product-001-${var.env}"].id #ingest02-data-product-001-sbox
+  name                       = each.value.full_name
+  resource_group_name        = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].name
+  location                   = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
+  service_plan_id            = azurerm_service_plan.example[each.key].id
+  storage_account_name       = data.azurerm_storage_account.xcutting[each.value.lz_key].name                       #azurerm_storage_account.example[each.key].name        
+  storage_account_access_key = data.azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key         #azurerm_storage_account.example[each.key].primary_access_key
+  virtual_network_subnet_id  = data.azurerm_subnet.lz["ingest${each.value.lz_key}-data-product-001-${var.env}"].id #ingest02-data-product-001-sbox
 
-#   app_settings = {
-#     APPLICATIONINSIGHTS_CONNECTION_STRING                 = azurerm_application_insights.example[each.key].connection_string
-#     AzureWebJobsFeatureFlags                              = "EnableWorkerIndexing"
-#     BUILD_FLAGS                                           = "UseExpressBuild"
-#     ENABLE_ORYX_BUILD                                     = true
-#     ENVIRONMENT                                           = var.env
-#     FUNCTIONS_WORKER_RUNTIME                              = "python"
-#     LZ_KEY                                                = each.value.lz_key
-#     PYTHON_ENABLE_WORKER_EXTENSIONS                       = 1
-#     sboxdlrmeventhubns_RootManageSharedAccessKey_EVENTHUB = data.azurerm_eventhub_namespace_authorization_rule.lz[each.value.lz_key].primary_connection_string
-#     SCM_DO_BUILD_DURING_DEPLOYMENT                        = 1
-#     XDG_CACHE_HOME                                        = "/tmp/.cache"
-#     WEBSITE_CONTENTSHARE                                  = each.value.full_name
-#     WEBSITE_CONTENTOVERVNET                               = "1"
-#     WEBSITE_RUN_FROM_PACKAGE                              = "1"
-#   }
+  app_settings = {
+    APPLICATIONINSIGHTS_CONNECTION_STRING                 = azurerm_application_insights.example[each.key].connection_string
+    AzureWebJobsFeatureFlags                              = "EnableWorkerIndexing"
+    BUILD_FLAGS                                           = "UseExpressBuild"
+    ENABLE_ORYX_BUILD                                     = true
+    ENVIRONMENT                                           = var.env
+    FUNCTIONS_WORKER_RUNTIME                              = "python"
+    LZ_KEY                                                = each.value.lz_key
+    PYTHON_ENABLE_WORKER_EXTENSIONS                       = 1
+    sboxdlrmeventhubns_RootManageSharedAccessKey_EVENTHUB = data.azurerm_eventhub_namespace_authorization_rule.lz[each.value.lz_key].primary_connection_string
+    SCM_DO_BUILD_DURING_DEPLOYMENT                        = 1
+    XDG_CACHE_HOME                                        = "/tmp/.cache"
+    # WEBSITE_CONTENTAZUREFILECONNECTIONSTRING              = each.value.connection_string
+    # WEBSITE_CONTENTSHARE                                  = each.value.full_name
+    WEBSITE_CONTENTOVERVNET                               = "1"
+    WEBSITE_RUN_FROM_PACKAGE                              = "1"
+  }
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   site_config {
-#     application_stack {
-#       python_version = "3.10"
-#     }
-#     always_on = false
+  site_config {
+    application_stack {
+      python_version = "3.10"
+    }
+    always_on = true
 
-#     scm_use_main_ip_restriction = false
-#     ftps_state                  = "FtpsOnly"
-#   }
+    scm_use_main_ip_restriction = false
+    ftps_state                  = "FtpsOnly"
+  }
 
-#   timeouts {
-#     read   = "10m"
-#     update = "40m"
-#   }
+  timeouts {
+    read   = "10m"
+    update = "40m"
+  }
 
-#   tags = module.ctags.common_tags
-# }
+  tags = module.ctags.common_tags
+}
 
 
 resource "azurerm_application_insights" "example" {
