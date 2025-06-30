03d3938964492886b8a346ead8dd7ec89e32988e
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-06-30 14:28:39 +0100

    test removing import block

--- components/aria-layer/import-remove.tf
+++ components/aria-layer/import-remove.tf
@@ -7,23 +7,23 @@
 # }
 
 #Define TF resources to import to state
-locals {
-  function_apps_import = {
-    "01-af-td"     = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest01-main-sbox/providers/Microsoft.Web/sites/af-td-sbox01-uks-dlrm-01"
-    "02-af-td"     = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-td-sbox02-uks-dlrm-01"
-    "02-af-joh"    = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-joh-sbox02-uks-dlrm-01"
-    "02-af-apluta" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-apluta-sbox02-uks-dlrm-01"
-    "00-af-bails"  = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-bails-sbox00-uks-dlrm-01"
-    "00-af-sbails" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-sbails-sbox00-uks-dlrm-01"
-    "02-af-sbails" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-sbails-sbox02-uks-dlrm-01"
-    "00-af-aplfpa" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-aplfpa-sbox00-uks-dlrm-01"
-    "00-af-aplfta" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-aplfta-sbox00-uks-dlrm-01"
-    "00-af-joh"    = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-joh-sbox00-uks-dlrm-01"
-  }
-}
+# locals {
+#   function_apps_import = {
+#     "01-af-td"     = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest01-main-sbox/providers/Microsoft.Web/sites/af-td-sbox01-uks-dlrm-01"
+#     "02-af-td"     = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-td-sbox02-uks-dlrm-01"
+#     "02-af-joh"    = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-joh-sbox02-uks-dlrm-01"
+#     "02-af-apluta" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-apluta-sbox02-uks-dlrm-01"
+#     "00-af-bails"  = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-bails-sbox00-uks-dlrm-01"
+#     "00-af-sbails" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-sbails-sbox00-uks-dlrm-01"
+#     "02-af-sbails" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest02-main-sbox/providers/Microsoft.Web/sites/af-sbails-sbox02-uks-dlrm-01"
+#     "00-af-aplfpa" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-aplfpa-sbox00-uks-dlrm-01"
+#     "00-af-aplfta" = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-aplfta-sbox00-uks-dlrm-01"
+#     "00-af-joh"    = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest00-main-sbox/providers/Microsoft.Web/sites/af-joh-sbox00-uks-dlrm-01"
+#   }
+# }
 
-import {
-  for_each = local.function_apps_import
-  to       = azurerm_linux_function_app.example[each.key]
-  id       = each.value
-}
+# import {
+#   for_each = local.function_apps_import
+#   to       = azurerm_linux_function_app.example[each.key]
+#   id       = each.value
+# }
--- components/aria-layer/private-endpoints.tf
+++ components/aria-layer/private-endpoints.tf
@@ -10,6 +10,23 @@ data "azurerm_private_dns_zone" "webapps" {
 }
 
 # Reference existing vnet and create link between DNS and vnet
+data "azurerm_private_dns_zone_virtual_network_link" "webapps_sbox" {
+  name                  = "ingest00-vnet-sbox" 
+  resource_group_name   = "ingest00-network-sbox"
+  private_dns_zone_name = "privatelink.azurewebsites.net"
+}
+
+
+#Reference non-delegated subnet
+data "azurerm_subnet" "pe" {
+  for_each = var.landing_zones
+
+  name                 = "ingest${each.key}-services${var.env}"
+  virtual_network_name = "ingest${each.key}-vnet-${var.env}"
+  resource_group_name  = "ingest${each.key}-network-${var.env}"
+}
+
+
 resource "azurerm_private_dns_zone_virtual_network_link" "webapps" {
   for_each = var.landing_zones
 
@@ -28,7 +45,7 @@ resource "azurerm_private_endpoint" "functionapp" {
   name                = "pe-${each.key}-functionapp"
   location            = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].location
   resource_group_name = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].name
-  subnet_id           = data.azurerm_subnet.lz["ingest${each.key}-data-product-001-${var.env}"].id
+  subnet_id           = data.azurerm_subnet.pe[each.key].id
 
   private_service_connection {
     name                           = "psc-${each.key}-blob"
