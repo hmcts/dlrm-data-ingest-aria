9712a2367144ba0cb450dae5e3e57bd0c1ae55a5
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-07-01 10:20:13 +0100

    linting fix

--- components/aria-layer/function_app.tf
+++ components/aria-layer/function_app.tf
@@ -45,8 +45,8 @@ resource "azurerm_linux_function_app" "example" {
     XDG_CACHE_HOME                                        = "/tmp/.cache"
     #WEBSITE_CONTENTAZUREFILECONNECTIONSTRING              = data.azurerm_storage_account.xcutting[each.value.lz_key].primary_connection_string
     #WEBSITE_CONTENTSHARE                                  = each.value.full_name
-    WEBSITE_CONTENTOVERVNET                               = "1"
-    WEBSITE_RUN_FROM_PACKAGE                              = "1"
+    WEBSITE_CONTENTOVERVNET  = "1"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
   }
 
   identity {
