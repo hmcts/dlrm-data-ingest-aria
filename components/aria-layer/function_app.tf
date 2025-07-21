6906f34f772847de5645370311fb04177400ed0d
AndrewMcDevittHMCTS <Andrew.McDevitt@HMCTS.NET>
2025-07-21 12:41:14 +0100

    delete functionapp. Recreate next round. kudu error

--- components/aria-layer/function_app.tf
+++ components/aria-layer/function_app.tf
@@ -15,72 +15,72 @@ resource "azurerm_service_plan" "example" {
   tags = module.ctags.common_tags
 }
 
-resource "azurerm_storage_account" "xcutting" {
-  for_each = var.landing_zones
+# resource "azurerm_storage_account" "xcutting" {
+#   for_each = var.landing_zones
 
-  name                = "ingest${each.key}xcutting${var.env}"
-  resource_group_name = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].name
+#   name                = "ingest${each.key}xcutting${var.env}"
+#   resource_group_name = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].name
 
-  location                 = "UK South"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+#   location                 = "UK South"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  network_rules {
-    default_action             = "Deny"                                                                       #Allow
-    virtual_network_subnet_ids = [data.azurerm_subnet.lz["ingest${each.key}-data-product-001-${var.env}"].id] #data.azurerm_subnet.lz["ingest${each.key}-data-product-001-${var.env}"].id] 
-  }
-}
+#   network_rules {
+#     default_action             = "Deny"                                                                       #Allow
+#     virtual_network_subnet_ids = [data.azurerm_subnet.lz["ingest${each.key}-data-product-001-${var.env}"].id] #data.azurerm_subnet.lz["ingest${each.key}-data-product-001-${var.env}"].id] 
+#   }
+# }
 
-resource "azurerm_linux_function_app" "example" {
-  for_each = {
-    for app in local.flattened_function_apps :
-    "${app.lz_key}-${app.base_name}" => app
-  }
+# resource "azurerm_linux_function_app" "example" {
+#   for_each = {
+#     for app in local.flattened_function_apps :
+#     "${app.lz_key}-${app.base_name}" => app
+#   }
 
-  name                       = each.value.full_name
-  resource_group_name        = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].name
-  location                   = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
-  service_plan_id            = azurerm_service_plan.example[each.key].id
-  storage_account_name       = azurerm_storage_account.xcutting[each.value.lz_key].name
-  storage_account_access_key = azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key
-  virtual_network_subnet_id  = data.azurerm_subnet.lz["ingest${each.value.lz_key}-data-product-001-${var.env}"].id
+#   name                       = each.value.full_name
+#   resource_group_name        = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].name
+#   location                   = data.azurerm_resource_group.lz["ingest${each.value.lz_key}-main-${var.env}"].location
+#   service_plan_id            = azurerm_service_plan.example[each.key].id
+#   storage_account_name       = azurerm_storage_account.xcutting[each.value.lz_key].name
+#   storage_account_access_key = azurerm_storage_account.xcutting[each.value.lz_key].primary_access_key
+#   virtual_network_subnet_id  = data.azurerm_subnet.lz["ingest${each.value.lz_key}-data-product-001-${var.env}"].id
 
-  app_settings = {
-    APPLICATIONINSIGHTS_CONNECTION_STRING                 = azurerm_application_insights.example[each.key].connection_string
-    AzureWebJobsFeatureFlags                              = "EnableWorkerIndexing"
-    AzureWebJobsStorage                                   = azurerm_storage_account.xcutting[each.value.lz_key].primary_connection_string
-    ENABLE_ORYX_BUILD                                     = true
-    ENVIRONMENT                                           = var.env
-    FUNCTIONS_EXTENSION_VERSION                           = "~4"
-    FUNCTIONS_WORKER_RUNTIME                              = "python"
-    LZ_KEY                                                = each.value.lz_key
-    PYTHON_ENABLE_WORKER_EXTENSIONS                       = 1
-    sboxdlrmeventhubns_RootManageSharedAccessKey_EVENTHUB = data.azurerm_eventhub_namespace_authorization_rule.lz[each.value.lz_key].primary_connection_string
-    SCM_DO_BUILD_DURING_DEPLOYMENT                        = 1
-    # XDG_CACHE_HOME                                        = "/tmp/.cache"
-    WEBSITE_RUN_FROM_PACKAGE = 1
-  }
+#   app_settings = {
+#     APPLICATIONINSIGHTS_CONNECTION_STRING                 = azurerm_application_insights.example[each.key].connection_string
+#     AzureWebJobsFeatureFlags                              = "EnableWorkerIndexing"
+#     AzureWebJobsStorage                                   = azurerm_storage_account.xcutting[each.value.lz_key].primary_connection_string
+#     ENABLE_ORYX_BUILD                                     = true
+#     ENVIRONMENT                                           = var.env
+#     FUNCTIONS_EXTENSION_VERSION                           = "~4"
+#     FUNCTIONS_WORKER_RUNTIME                              = "python"
+#     LZ_KEY                                                = each.value.lz_key
+#     PYTHON_ENABLE_WORKER_EXTENSIONS                       = 1
+#     sboxdlrmeventhubns_RootManageSharedAccessKey_EVENTHUB = data.azurerm_eventhub_namespace_authorization_rule.lz[each.value.lz_key].primary_connection_string
+#     SCM_DO_BUILD_DURING_DEPLOYMENT                        = 1
+#     # XDG_CACHE_HOME                                        = "/tmp/.cache"
+#     WEBSITE_RUN_FROM_PACKAGE = 1
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  site_config {
-    application_stack {
-      python_version = "3.11"
-    }
+#   site_config {
+#     application_stack {
+#       python_version = "3.11"
+#     }
 
-    always_on = true
-  }
+#     always_on = true
+#   }
 
-  # https_only = true
+#   # https_only = true
 
-  tags = module.ctags.common_tags
+#   tags = module.ctags.common_tags
 
-  depends_on = [
-    azurerm_storage_account.xcutting
-  ]
-}
+#   depends_on = [
+#     azurerm_storage_account.xcutting
+#   ]
+# }
 
 resource "azurerm_application_insights" "example" {
   for_each = {
