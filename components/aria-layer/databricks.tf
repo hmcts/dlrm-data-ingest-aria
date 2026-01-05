5046c898467d3b16e73d32ca89a95b004c1095bd
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-01-05 11:28:24 +0000

    uncomment provider. looks like we need to retain provider to delete resources referenced by provider

--- components/aria-layer/databricks.tf
+++ components/aria-layer/databricks.tf
@@ -41,17 +41,17 @@ provider "databricks" {
   skip_verify = var.env != "sbox"
 }
 
-# provider "databricks" {
-#   alias                       = "sbox-01"
-#   azure_workspace_resource_id = try(data.azurerm_databricks_workspace.db_ws["sbox-01"].id, null)
-#   host                        = try(data.azurerm_databricks_workspace.db_ws["sbox-01"].workspace_url, null)
+provider "databricks" {
+  alias                       = "sbox-01"
+  azure_workspace_resource_id = try(data.azurerm_databricks_workspace.db_ws["sbox-01"].id, null)
+  host                        = try(data.azurerm_databricks_workspace.db_ws["sbox-01"].workspace_url, null)
 
-#   azure_client_id     = data.azurerm_client_config.current.client_id
-#   azure_client_secret = data.azurerm_key_vault_secret.client_secret.value
-#   azure_tenant_id     = data.azurerm_client_config.current.tenant_id
+  azure_client_id     = data.azurerm_client_config.current.client_id
+  azure_client_secret = data.azurerm_key_vault_secret.client_secret.value
+  azure_tenant_id     = data.azurerm_client_config.current.tenant_id
 
-#   skip_verify = var.env != "sbox"
-# }
+  skip_verify = var.env != "sbox"
+}
 
 provider "databricks" {
   alias                       = "sbox-02"
