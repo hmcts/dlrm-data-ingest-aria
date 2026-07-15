b1a2341e31e5641d11fc2759d66ff3ec5b7b9084
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-07-15 13:59:23 +0100

    update metadata resource names

--- components/aria-layer/access_policy.tf
+++ components/aria-layer/access_policy.tf
@@ -217,7 +217,7 @@ resource "azurerm_databricks_access_connector" "ext_access_connector" {
   }
 }
 
-resource "azurerm_role_assignment" "ext_storage_2" {
+resource "azurerm_role_assignment" "curated_uc" {
   for_each = var.landing_zones
 
   scope                = data.azurerm_storage_account.curated[each.value.lz_key].name
@@ -225,18 +225,26 @@ resource "azurerm_role_assignment" "ext_storage_2" {
   principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "ext_storage_2" {
+resource "azurerm_role_assignment" "external_uc" {
   for_each = var.landing_zones
 
-  scope                = data.azurerm_storage_account.curated[each.value.lz_key].name
+  scope                = data.azurerm_storage_account.external[each.value.lz_key].name
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "ext_storage_2" {
+resource "azurerm_role_assignment" "raw_uc" {
   for_each = var.landing_zones
 
-  scope                = data.azurerm_storage_account.curated[each.value.lz_key].name
+  scope                = data.azurerm_storage_account.raw[each.value.lz_key].name
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "landing_uc" {
+  for_each = var.landing_zones
+
+  scope                = data.azurerm_storage_account.landing[each.value.lz_key].name
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_databricks_access_connector.ext_access_connector.identity[0].principal_id
 }
