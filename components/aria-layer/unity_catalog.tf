1b69fc53275ce41270e91357bd0b81f7150d5263
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-07-15 13:42:57 +0100

    remove metastore data block as metastore_id (account level) provided in var.

--- components/aria-layer/unity_catalog.tf
+++ components/aria-layer/unity_catalog.tf
@@ -1,11 +1,6 @@
-data "databricks_metastore" "sbox00" {
-  provider     = databricks.sbox-00
-  metastore_id = var.metastore_id
-}
-
 resource "databricks_metastore_assignment" "sbox00" {
   provider     = databricks.sbox-00
-  workspace_id = data.azurerm_databricks_workspace.db_ws["sbox-00"].id
+  workspace_id = data.azurerm_databricks_workspace.db_ws["sbox-00"].workspace_id
   metastore_id = var.metastore_id
 }
 
