b0d34d1085a8ce88dc429f69d87913d422c0534e
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-07-16 12:04:13 +0100

    add time_sleep

--- components/aria-layer/unity_catalog.tf
+++ components/aria-layer/unity_catalog.tf
@@ -486,7 +486,7 @@ resource "databricks_catalog" "aria_catalog_sbox00" {
   isolation_mode = "ISOLATED"
 
   depends_on = [
-    time_sleep.wait_for_metastore_assignment,
+    databricks_metastore_assignment.sbox00,
     databricks_external_location.landing_external_sbox00,
     databricks_grants.metastore_grants_sbox00
   ]
@@ -506,7 +506,7 @@ resource "databricks_catalog" "aria_catalog_stg00" {
   isolation_mode = "ISOLATED"
 
   depends_on = [
-    time_sleep.wait_for_metastore_assignment,
+    databricks_metastore_assignment.stg00,
     databricks_external_location.landing_external_stg00,
     databricks_grants.metastore_grants_stg00
   ]
@@ -526,7 +526,7 @@ resource "databricks_catalog" "aria_catalog_stg01" {
   isolation_mode = "ISOLATED"
 
   depends_on = [
-    time_sleep.wait_for_metastore_assignment,
+    databricks_metastore_assignment.stg01,
     databricks_external_location.landing_external_stg01,
     databricks_grants.metastore_grants_stg01
   ]
@@ -546,15 +546,10 @@ resource "databricks_catalog" "aria_catalog_prod00" {
   isolation_mode = "ISOLATED"
 
   depends_on = [
-    time_sleep.wait_for_metastore_assignment,
+    databricks_metastore_assignment.prod00,
     databricks_external_location.landing_external_prod00,
     databricks_grants.metastore_grants_prod00
   ]
-}
-
-resource "time_sleep" "wait_for_metastore_assignment" {
-  depends_on      = [databricks_metastore_assignment]
-  create_duration = "30s"
 }
 
 ## storage credentials
@@ -569,7 +564,9 @@ resource "databricks_storage_credential" "external_sbox00" {
   isolation_mode = "ISOLATION_MODE_ISOLATED"
   comment        = "Managed by TF"
 
-  depends_on = [time_sleep.wait_for_metastore_assignment]
+#   depends_on = [databricks_metastore_assignment.sbox00]
+  depends_on = [time_sleep.wait_for_uc]
+
 }
 
 resource "databricks_storage_credential" "external_stg00" {
@@ -583,7 +580,7 @@ resource "databricks_storage_credential" "external_stg00" {
   isolation_mode = "ISOLATION_MODE_ISOLATED"
   comment        = "Managed by TF"
 
-  depends_on = [time_sleep.wait_for_metastore_assignment]
+  depends_on = [databricks_metastore_assignment.stg00]
 }
 
 resource "databricks_storage_credential" "external_stg01" {
@@ -597,7 +594,7 @@ resource "databricks_storage_credential" "external_stg01" {
   isolation_mode = "ISOLATION_MODE_ISOLATED"
   comment        = "Managed by TF"
 
-  depends_on = [time_sleep.wait_for_metastore_assignment]
+  depends_on = [databricks_metastore_assignment.stg01]
 }
 
 resource "databricks_storage_credential" "external_prod00" {
@@ -611,7 +608,7 @@ resource "databricks_storage_credential" "external_prod00" {
   isolation_mode = "ISOLATION_MODE_ISOLATED"
   comment        = "Managed by TF"
 
-  depends_on = [time_sleep.wait_for_metastore_assignment]
+  depends_on = [databricks_metastore_assignment.prod00]
 }
 
 ## external locations
@@ -858,5 +855,11 @@ resource "databricks_grants" "catalog_aria_grants_prod00" {
   }
 }
 
+resource "time_sleep" "wait_for_uc" {
+  depends_on = [
+    databricks_metastore_assignment.sbox00
+  ]
 
+  create_duration = "90s"
+}
 
