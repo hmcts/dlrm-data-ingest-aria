48db483abfe08e453e41e315b9c59cdbadb86234
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-07-16 16:37:42 +0100

    test using only sbox00 permissions for uc

--- components/aria-layer/unity_catalog.tf
+++ components/aria-layer/unity_catalog.tf
@@ -10,97 +10,144 @@ data "databricks_metastore" "this" {
   metastore_id = var.metastore_id
 }
 
-data "azurerm_databricks_workspace" "db_ws" {
-  name                = "ingest00-product-databricks001-${var.env}"
-  resource_group_name = "ingest00-main-${var.env}"
-}
+##sbox00
 
-provider "databricks" {
-  host = data.azurerm_databricks_workspace.db_ws.workspace_url
-}
-
-resource "databricks_group" "aria_admins" {
-  provider     = databricks.account
-  display_name = "aria_admin_${var.env}"
-}
-
-resource "databricks_catalog" "aria_catalog" {
-  for_each = var.landing_zones
-
-  name    = "aria_${var.env}${each.key}"
-  comment = "this catalog is managed by terraform"
-  properties = {
-    purpose = "Aria catalog for ${var.env}${each.key}"
-  }
-
-  storage_root   = "abfss://landing@ingest${each.key}landing${var.env}.dfs.core.windows.net/aria_uc_${var.env}"
-  isolation_mode = "ISOLATED"
-}
-
+# Access connector
 resource "azurerm_databricks_access_connector" "ext_access_connector" {
-  for_each = var.landing_zones
 
-  name                = "${var.env}${each.key}-ext-access-connector"
-  resource_group_name = "ingest${each.key}-main-${var.env}"
-  location            = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].location
+  name                = "${var.env}00-ext-access-connector"
+  resource_group_name = "ingest00-main-${var.env}"
+  location            = data.azurerm_resource_group.lz["ingest00-main-${var.env}"].location
 
   identity {
     type = "SystemAssigned"
   }
 }
 
-resource "databricks_storage_credential" "external" {
-  for_each = var.landing_zones
 
-  name = "aria_catalog_${var.env}${each.key}"
+# Storage credential
+resource "databricks_storage_credential" "external" {
+
+  provider = databricks.sbox-00
+  name     = "aria_catalog_${var.env}00"
+
   azure_managed_identity {
-    access_connector_id = azurerm_databricks_access_connector.ext_access_connector[each.key].id
+    access_connector_id = azurerm_databricks_access_connector.ext_access_connector.id
   }
+
   isolation_mode = "ISOLATION_MODE_ISOLATED"
   comment        = "Managed by TF"
 }
 
-resource "databricks_external_location" "landing_external" {
-  for_each = var.landing_zones
 
-  name = "external_storage_aria_uc_${var.env}${each.key}"
+# External location
+resource "databricks_external_location" "landing_external" {
+
+  provider = databricks.sbox-00
+  name     = "external_storage_aria_uc_${var.env}00"
   url = format(
     "abfss://%s@%s.dfs.core.windows.net",
     "landing",
-    data.azurerm_storage_account.landing[each.key].name
+    data.azurerm_storage_account.landing["00"].name
   )
-  credential_name = databricks_storage_credential.external[each.key].id
-  comment         = "Managed by TF "
-  isolation_mode  = "ISOLATION_MODE_ISOLATED"
+
+  credential_name = databricks_storage_credential.external.id
+
+  comment        = "Managed by TF"
+  isolation_mode = "ISOLATED"
 }
 
-## perms
+# Catalog
+resource "databricks_catalog" "aria_catalog" {
+
+  provider = databricks.sbox-00
+
+  name = "aria_${var.env}00"
+
+  comment = "this catalog is managed by terraform"
+
+  properties = {
+    purpose = "Aria catalog for ${var.env}00"
+  }
+
+  storage_root = "abfss://landing@ingest00landing${var.env}.dfs.core.windows.net/aria_uc_${var.env}"
+
+  isolation_mode = "ISOLATED"
+}
+
+# Storage credential permissions
 resource "databricks_grants" "storage_cred_grants" {
-  for_each           = var.landing_zones
-  storage_credential = databricks_storage_credential.external[each.key].id
+
+  provider = databricks.sbox-00
+
+  storage_credential = databricks_storage_credential.external.id
 
   grant {
-    principal  = databricks_group.aria_admins.display_name
-    privileges = ["ALL_PRIVILEGES", "MANAGE"]
+    principal = databricks_group.aria_admins.display_name
+
+    privileges = [
+      "ALL_PRIVILEGES",
+      "MANAGE"
+    ]
   }
 }
 
+# External location permissions
 resource "databricks_grants" "external_location_admin_grants" {
-  for_each          = var.landing_zones
-  external_location = databricks_external_location.landing_external[each.key].id
+
+  provider = databricks.sbox-00
+
+  external_location = databricks_external_location.landing_external.id
 
   grant {
-    principal  = databricks_group.aria_admins.display_name
-    privileges = ["ALL_PRIVILEGES", "MANAGE"]
+    principal = databricks_group.aria_admins.display_name
+
+    privileges = [
+      "ALL_PRIVILEGES",
+      "MANAGE"
+    ]
   }
 }
 
+# Catalog permissions
 resource "databricks_grants" "catalog_aria_grants" {
-  for_each = var.landing_zones
-  catalog  = databricks_catalog.aria_catalog[each.key].name
+
+  provider = databricks.sbox-00
+
+  catalog = databricks_catalog.aria_catalog.name
 
   grant {
-    principal  = databricks_group.aria_admins.display_name
-    privileges = ["ALL_PRIVILEGES"]
+    principal = databricks_group.aria_admins.display_name
+
+    privileges = [
+      "ALL_PRIVILEGES"
+    ]
   }
 }
+
+##stg00
+
+
+
+
+
+
+
+
+
+##stg01
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#prod00
