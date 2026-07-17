1dafdf972fc1b056b3e2a9fabf555297eac0b0dc
AndrewHMCTS <andrewmcdevitt@hmcts.net>
2026-07-17 14:09:40 +0100

    redeploy

--- components/aria-layer/unity_catalog.tf
+++ components/aria-layer/unity_catalog.tf
@@ -4,251 +4,251 @@ provider "databricks" {
   account_id = var.databricks_account_id
 }
 
-# data "databricks_metastore" "this" {
-#   count        = var.assign_account == "true" ? 1 : 0
-#   provider     = databricks.account
-#   metastore_id = var.metastore_id
-# }
+data "databricks_metastore" "this" {
+  count        = var.assign_account == "true" ? 1 : 0
+  provider     = databricks.account
+  metastore_id = var.metastore_id
+}
 
-# resource "databricks_group" "aria_admins" {
-#   provider     = databricks.account
-#   display_name = "aria_admin_${var.env}"
-# }
+resource "databricks_group" "aria_admins" {
+  provider     = databricks.account
+  display_name = "aria_admin_${var.env}"
+}
 
-# data "databricks_user" "aria_admins" {
-#   provider = databricks.account
+data "databricks_user" "aria_admins" {
+  provider = databricks.account
 
-#   for_each = toset(var.aria_uc_admins)
+  for_each = toset(var.aria_uc_admins)
 
-#   user_name = each.value
-# }
+  user_name = each.value
+}
 
-# resource "databricks_group_member" "aria_admins" {
-#   provider = databricks.account
+resource "databricks_group_member" "aria_admins" {
+  provider = databricks.account
 
-#   for_each = data.databricks_user.aria_admins
+  for_each = data.databricks_user.aria_admins
 
-#   group_id  = databricks_group.aria_admins.id
-#   member_id = each.value.id
-# }
+  group_id  = databricks_group.aria_admins.id
+  member_id = each.value.id
+}
 
-# # Access connector
-# resource "azurerm_databricks_access_connector" "ext_access_connector" {
+# Access connector
+resource "azurerm_databricks_access_connector" "ext_access_connector" {
 
-#   for_each = var.landing_zones
+  for_each = var.landing_zones
 
-#   name                = "${var.env}${each.key}-ext-access-connector"
-#   resource_group_name = "ingest${each.key}-main-${var.env}"
-#   location            = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].location
+  name                = "${var.env}${each.key}-ext-access-connector"
+  resource_group_name = "ingest${each.key}-main-${var.env}"
+  location            = data.azurerm_resource_group.lz["ingest${each.key}-main-${var.env}"].location
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
-# ##sbox00
+##sbox00
 
-# ##metastore assignment
-# resource "databricks_metastore_assignment" "workspace_00" {
-#   provider = databricks.account
+##metastore assignment
+resource "databricks_metastore_assignment" "workspace_00" {
+  provider = databricks.account
 
-#   workspace_id = data.azurerm_databricks_workspace.db_ws["sbox-00"].workspace_id
-#   metastore_id = var.metastore_id
+  workspace_id = data.azurerm_databricks_workspace.db_ws["sbox-00"].workspace_id
+  metastore_id = var.metastore_id
 
-#   # Optional but recommended
-#   #   default_catalog_name = "main"
-# }
-
-
-# # Storage credential
-# resource "databricks_storage_credential" "external_sbox00" {
-
-#   provider = databricks.sbox-00
-#   name     = "aria_catalog_${var.env}00"
-
-#   azure_managed_identity {
-#     access_connector_id = azurerm_databricks_access_connector.ext_access_connector["00"].id
-#   }
-
-#   isolation_mode = "ISOLATION_MODE_ISOLATED"
-#   comment        = "Managed by TF"
-# }
+  # Optional but recommended
+  #   default_catalog_name = "main"
+}
 
 
-# # External location
-# resource "databricks_external_location" "landing_external_sbox00" {
+# Storage credential
+resource "databricks_storage_credential" "external_sbox00" {
 
-#   provider = databricks.sbox-00
-#   name     = "external_storage_aria_uc_${var.env}00"
-#   url = format(
-#     "abfss://%s@%s.dfs.core.windows.net/aria_uc",
-#     "landing",
-#     data.azurerm_storage_account.landing["00"].name
-#   )
+  provider = databricks.sbox-00
+  name     = "aria_catalog_${var.env}00"
 
-#   credential_name = databricks_storage_credential.external_sbox00.id
+  azure_managed_identity {
+    access_connector_id = azurerm_databricks_access_connector.ext_access_connector["00"].id
+  }
 
-#   comment        = "Managed by TF"
-#   isolation_mode = "ISOLATED"
+  isolation_mode = "ISOLATION_MODE_ISOLATED"
+  comment        = "Managed by TF"
+}
 
-#   depends_on = [
-#     databricks_metastore_assignment.workspace_00
-#   ]
-# }
 
-# # Catalog
-# resource "databricks_catalog" "aria_catalog_sbox00" {
+# External location
+resource "databricks_external_location" "landing_external_sbox00" {
 
-#   provider = databricks.sbox-00
+  provider = databricks.sbox-00
+  name     = "external_storage_aria_uc_${var.env}00"
+  url = format(
+    "abfss://%s@%s.dfs.core.windows.net/aria_uc",
+    "landing",
+    data.azurerm_storage_account.landing["00"].name
+  )
 
-#   name = "aria_${var.env}00"
+  credential_name = databricks_storage_credential.external_sbox00.id
 
-#   comment = "this catalog is managed by terraform"
+  comment        = "Managed by TF"
+  isolation_mode = "ISOLATED"
 
-#   properties = {
-#     purpose = "Aria catalog for ${var.env}00"
-#   }
+  depends_on = [
+    databricks_metastore_assignment.workspace_00
+  ]
+}
 
-#   storage_root = "abfss://landing@ingest00landing${var.env}.dfs.core.windows.net/aria_uc"
+# Catalog
+resource "databricks_catalog" "aria_catalog_sbox00" {
 
-#   isolation_mode = "ISOLATED"
+  provider = databricks.sbox-00
 
-#   depends_on = [
-#     databricks_external_location.landing_external_sbox00
-#   ]
-# }
+  name = "aria_${var.env}00"
 
-# #create active schema
-# resource "databricks_schema" "active_sbox00" {
-#   provider     = databricks.sbox-00
+  comment = "this catalog is managed by terraform"
+
+  properties = {
+    purpose = "Aria catalog for ${var.env}00"
+  }
+
+  storage_root = "abfss://landing@ingest00landing${var.env}.dfs.core.windows.net/aria_uc"
+
+  isolation_mode = "ISOLATED"
+
+  depends_on = [
+    databricks_external_location.landing_external_sbox00
+  ]
+}
+
+#create active schema
+resource "databricks_schema" "active_sbox00" {
+  provider     = databricks.sbox-00
+  catalog_name = databricks_catalog.aria_catalog_sbox00.id
+  name         = "active"
+  comment      = "this database is managed by terraform"
+  properties = {
+    kind = "various"
+  }
+}
+
+# #create archive schema
+# resource "databricks_schema" "archive_sbox00" {
 #   catalog_name = databricks_catalog.aria_catalog_sbox00.id
-#   name         = "active"
+#   name         = "archive"
 #   comment      = "this database is managed by terraform"
 #   properties = {
 #     kind = "various"
 #   }
 # }
 
-# # #create archive schema
-# # resource "databricks_schema" "archive_sbox00" {
-# #   catalog_name = databricks_catalog.aria_catalog_sbox00.id
-# #   name         = "archive"
-# #   comment      = "this database is managed by terraform"
-# #   properties = {
-# #     kind = "various"
-# #   }
-# # }
+# Storage credential permissions
+resource "databricks_grants" "storage_cred_grants_sbox00" {
 
-# # Storage credential permissions
-# resource "databricks_grants" "storage_cred_grants_sbox00" {
+  provider = databricks.sbox-00
 
-#   provider = databricks.sbox-00
+  storage_credential = databricks_storage_credential.external_sbox00.id
 
-#   storage_credential = databricks_storage_credential.external_sbox00.id
+  grant {
+    principal = databricks_group.aria_admins.display_name
 
-#   grant {
-#     principal = databricks_group.aria_admins.display_name
+    privileges = [
+      "ALL_PRIVILEGES",
+      "MANAGE"
+    ]
+  }
+}
 
-#     privileges = [
-#       "ALL_PRIVILEGES",
-#       "MANAGE"
-#     ]
-#   }
-# }
+# External location permissions
+resource "databricks_grants" "external_location_admin_grants_sbox00" {
 
-# # External location permissions
-# resource "databricks_grants" "external_location_admin_grants_sbox00" {
+  provider = databricks.sbox-00
 
-#   provider = databricks.sbox-00
+  external_location = databricks_external_location.landing_external_sbox00.id
 
-#   external_location = databricks_external_location.landing_external_sbox00.id
+  grant {
+    principal = databricks_group.aria_admins.display_name
 
-#   grant {
-#     principal = databricks_group.aria_admins.display_name
+    privileges = [
+      "ALL_PRIVILEGES",
+      "MANAGE"
+    ]
+  }
+}
 
-#     privileges = [
-#       "ALL_PRIVILEGES",
-#       "MANAGE"
-#     ]
-#   }
-# }
+# Catalog permissions
+resource "databricks_grants" "sbox00_catalog" {
+  provider = databricks.sbox-00
+  catalog  = databricks_catalog.aria_catalog_sbox00.name
+  grant {
+    principal = data.azurerm_client_config.current.client_id
 
-# # Catalog permissions
-# resource "databricks_grants" "sbox00_catalog" {
-#   provider = databricks.sbox-00
-#   catalog  = databricks_catalog.aria_catalog_sbox00.name
-#   grant {
-#     principal = data.azurerm_client_config.current.client_id
+    privileges = [
+      "USE_CATALOG",
+      "CREATE_SCHEMA",
+      "CREATE_TABLE",
+      "SELECT",
+      "MODIFY",
+      "BROWSE",
+      "USE_SCHEMA"
+    ]
+  }
+  grant {
+    principal = databricks_group.aria_admins.display_name
 
-#     privileges = [
-#       "USE_CATALOG",
-#       "CREATE_SCHEMA",
-#       "CREATE_TABLE",
-#       "SELECT",
-#       "MODIFY",
-#       "BROWSE",
-#       "USE_SCHEMA"
-#     ]
-#   }
-#   grant {
-#     principal = databricks_group.aria_admins.display_name
+    privileges = [
+      "USE_CATALOG",
+      "CREATE_SCHEMA",
+      "CREATE_TABLE",
+      "SELECT",
+      "MODIFY",
+      "BROWSE",
+      "USE_SCHEMA"
+    ]
+  }
+}
 
-#     privileges = [
-#       "USE_CATALOG",
-#       "CREATE_SCHEMA",
-#       "CREATE_TABLE",
-#       "SELECT",
-#       "MODIFY",
-#       "BROWSE",
-#       "USE_SCHEMA"
-#     ]
-#   }
-# }
+##assign permissions for user to create external location on the metastore
+resource "databricks_grants" "metastore_sbox00" {
+  provider  = databricks.sbox-00
+  metastore = var.metastore_id
 
-# ##assign permissions for user to create external location on the metastore
-# resource "databricks_grants" "metastore_sbox00" {
-#   provider  = databricks.sbox-00
-#   metastore = var.metastore_id
+  grant {
+    principal = data.azurerm_client_config.current.client_id
 
-#   grant {
-#     principal = data.azurerm_client_config.current.client_id
+    privileges = [
+      "CREATE_CATALOG",
+      "CREATE_EXTERNAL_LOCATION"
+    ]
+  }
+}
 
-#     privileges = [
-#       "CREATE_CATALOG",
-#       "CREATE_EXTERNAL_LOCATION"
-#     ]
-#   }
-# }
+resource "databricks_grants" "active_schema" {
+  provider = databricks.sbox-00
 
-# resource "databricks_grants" "active_schema" {
-#   provider = databricks.sbox-00
+  schema = "${databricks_catalog.aria_catalog_sbox00.name}.active"
 
-#   schema = "${databricks_catalog.aria_catalog_sbox00.name}.active"
+  grant {
+    principal = databricks_group.aria_admins.display_name
 
-#   grant {
-#     principal = databricks_group.aria_admins.display_name
+    privileges = [
+      "USE_SCHEMA",
+      "CREATE_TABLE",
+      "MODIFY",
+      "SELECT"
+    ]
+  }
 
-#     privileges = [
-#       "USE_SCHEMA",
-#       "CREATE_TABLE",
-#       "MODIFY",
-#       "SELECT"
-#     ]
-#   }
+  grant {
+    principal = data.azurerm_client_config.current.client_id
 
-#   grant {
-#     principal = data.azurerm_client_config.current.client_id
+    privileges = [
+      "USE_SCHEMA",
+      "CREATE_TABLE",
+      "MODIFY",
+      "SELECT"
+    ]
+  }
+}
 
-#     privileges = [
-#       "USE_SCHEMA",
-#       "CREATE_TABLE",
-#       "MODIFY",
-#       "SELECT"
-#     ]
-#   }
-# }
-
-# ##stg00
+##stg00
 
 
 
@@ -258,7 +258,7 @@ provider "databricks" {
 
 
 
-# ##stg01
+##stg01
 
 
 
@@ -273,5 +273,5 @@ provider "databricks" {
 
 
 
-# #prod00
+#prod00
 
